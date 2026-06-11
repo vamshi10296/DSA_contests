7e2aec045efe05091553a69ffbe33658447c37ff
vamshi <yvamshi858@gmail.com>
2026-06-11 12:21:49 +0530

    Add files via upload

---
+++ contestQues1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    
+    if(x % 5 == 0){
+        cout<< " No ";
+    }
+    else{
+        cout<<" Yes ";
+    }
+    return 0;
+ 
+
+}
